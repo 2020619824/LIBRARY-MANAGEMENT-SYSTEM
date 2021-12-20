eb6df783e3001dbeeb9ac309daf54f1b5ac22831
AHMAD NASRUDDIN BIN AHMAD NARASHID <2020619824@isiswa.uitm.edu.my>
2021-12-20 10:34:47 +0800

    Book details

--- LIBRARY MANAGEMENT SYSTEM/Book Details.Designer.vb
+++ LIBRARY MANAGEMENT SYSTEM/Book Details.Designer.vb
@@ -22,16 +22,29 @@ Partial Class BookDetails
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
         Me.SuspendLayout()
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Location = New System.Drawing.Point(37, 36)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(602, 71)
+        Me.GroupBox1.TabIndex = 0
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Search"
         '
         'BookDetails
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.GroupBox1)
         Me.Name = "BookDetails"
         Me.Text = "Book Details"
         Me.ResumeLayout(False)
 
     End Sub
+
+    Friend WithEvents GroupBox1 As GroupBox
 End Class
