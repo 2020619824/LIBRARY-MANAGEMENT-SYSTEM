867857b0052d2160fcb4d6a62f900c53d14be2a0
NURUL SYAQIRA BINTI ZAMRIDIN NURUL SYAQIRA BINTI ZAMRIDIN <2020457902@isiswa.uitm.edu.my>
2022-01-24 21:45:08 +0800

    edit registration

--- LIBRARY MANAGEMENT SYSTEM/Registration.Designer.vb
+++ LIBRARY MANAGEMENT SYSTEM/Registration.Designer.vb
@@ -23,6 +23,11 @@ Partial Class Registration
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
         Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.txtPassword = New System.Windows.Forms.TextBox()
+        Me.txtUsername = New System.Windows.Forms.TextBox()
+        Me.txtPhoneNumber = New System.Windows.Forms.TextBox()
+        Me.txtStaffID = New System.Windows.Forms.TextBox()
+        Me.txtName = New System.Windows.Forms.TextBox()
         Me.Label6 = New System.Windows.Forms.Label()
         Me.Label5 = New System.Windows.Forms.Label()
         Me.Label4 = New System.Windows.Forms.Label()
@@ -31,18 +36,11 @@ Partial Class Registration
         Me.Label1 = New System.Windows.Forms.Label()
         Me.btnReturn = New System.Windows.Forms.Button()
         Me.btnRegister = New System.Windows.Forms.Button()
-        Me.txtName = New System.Windows.Forms.TextBox()
-        Me.txtStaffID = New System.Windows.Forms.TextBox()
-        Me.txtPhoneNumber = New System.Windows.Forms.TextBox()
-        Me.txtUsername = New System.Windows.Forms.TextBox()
-        Me.txtPassword = New System.Windows.Forms.TextBox()
-        Me.Label7 = New System.Windows.Forms.Label()
         Me.GroupBox1.SuspendLayout()
         Me.SuspendLayout()
         '
         'GroupBox1
         '
-        Me.GroupBox1.Controls.Add(Me.Label7)
         Me.GroupBox1.Controls.Add(Me.txtPassword)
         Me.GroupBox1.Controls.Add(Me.txtUsername)
         Me.GroupBox1.Controls.Add(Me.txtPhoneNumber)
@@ -53,141 +51,140 @@ Partial Class Registration
         Me.GroupBox1.Controls.Add(Me.Label4)
         Me.GroupBox1.Controls.Add(Me.Label3)
         Me.GroupBox1.Controls.Add(Me.Label2)
-        Me.GroupBox1.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.GroupBox1.Location = New System.Drawing.Point(29, 49)
+        Me.GroupBox1.Font = New System.Drawing.Font("Arial Black", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.GroupBox1.ForeColor = System.Drawing.Color.White
+        Me.GroupBox1.Location = New System.Drawing.Point(54, 89)
         Me.GroupBox1.Name = "GroupBox1"
-        Me.GroupBox1.Size = New System.Drawing.Size(658, 317)
+        Me.GroupBox1.Size = New System.Drawing.Size(597, 269)
         Me.GroupBox1.TabIndex = 3
         Me.GroupBox1.TabStop = False
         Me.GroupBox1.Text = "User Details"
         '
+        'txtPassword
+        '
+        Me.txtPassword.Location = New System.Drawing.Point(118, 214)
+        Me.txtPassword.Name = "txtPassword"
+        Me.txtPassword.Size = New System.Drawing.Size(305, 30)
+        Me.txtPassword.TabIndex = 9
+        '
+        'txtUsername
+        '
+        Me.txtUsername.Location = New System.Drawing.Point(118, 168)
+        Me.txtUsername.Name = "txtUsername"
+        Me.txtUsername.Size = New System.Drawing.Size(305, 30)
+        Me.txtUsername.TabIndex = 8
+        '
+        'txtPhoneNumber
+        '
+        Me.txtPhoneNumber.Location = New System.Drawing.Point(118, 122)
+        Me.txtPhoneNumber.Name = "txtPhoneNumber"
+        Me.txtPhoneNumber.Size = New System.Drawing.Size(305, 30)
+        Me.txtPhoneNumber.TabIndex = 7
+        '
+        'txtStaffID
+        '
+        Me.txtStaffID.Location = New System.Drawing.Point(118, 75)
+        Me.txtStaffID.Name = "txtStaffID"
+        Me.txtStaffID.Size = New System.Drawing.Size(305, 30)
+        Me.txtStaffID.TabIndex = 6
+        '
+        'txtName
+        '
+        Me.txtName.Location = New System.Drawing.Point(118, 28)
+        Me.txtName.Name = "txtName"
+        Me.txtName.Size = New System.Drawing.Size(305, 30)
+        Me.txtName.TabIndex = 5
+        '
         'Label6
         '
         Me.Label6.AutoSize = True
-        Me.Label6.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label6.Location = New System.Drawing.Point(43, 221)
+        Me.Label6.Font = New System.Drawing.Font("Arial Narrow", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label6.Location = New System.Drawing.Point(51, 220)
         Me.Label6.Name = "Label6"
-        Me.Label6.Size = New System.Drawing.Size(86, 20)
+        Me.Label6.Size = New System.Drawing.Size(61, 16)
         Me.Label6.TabIndex = 4
         Me.Label6.Text = "Password :"
         '
         'Label5
         '
         Me.Label5.AutoSize = True
-        Me.Label5.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label5.Location = New System.Drawing.Point(38, 154)
+        Me.Label5.Font = New System.Drawing.Font("Arial Narrow", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label5.Location = New System.Drawing.Point(49, 174)
         Me.Label5.Name = "Label5"
-        Me.Label5.Size = New System.Drawing.Size(91, 20)
+        Me.Label5.Size = New System.Drawing.Size(63, 16)
         Me.Label5.TabIndex = 3
         Me.Label5.Text = "Username :"
         '
         'Label4
         '
         Me.Label4.AutoSize = True
-        Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label4.Location = New System.Drawing.Point(6, 113)
+        Me.Label4.Font = New System.Drawing.Font("Arial Narrow", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.Location = New System.Drawing.Point(26, 128)
         Me.Label4.Name = "Label4"
-        Me.Label4.Size = New System.Drawing.Size(123, 20)
+        Me.Label4.Size = New System.Drawing.Size(86, 16)
         Me.Label4.TabIndex = 2
         Me.Label4.Text = "Phone Number :"
         '
         'Label3
         '
         Me.Label3.AutoSize = True
-        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label3.Location = New System.Drawing.Point(56, 72)
+        Me.Label3.Font = New System.Drawing.Font("Arial Narrow", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(65, 81)
         Me.Label3.Name = "Label3"
-        Me.Label3.Size = New System.Drawing.Size(73, 20)
+        Me.Label3.Size = New System.Drawing.Size(47, 16)
         Me.Label3.TabIndex = 1
         Me.Label3.Text = "Staff ID :"
         '
         'Label2
         '
         Me.Label2.AutoSize = True
-        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Font = New System.Drawing.Font("Arial Narrow", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.Label2.Location = New System.Drawing.Point(70, 31)
         Me.Label2.Name = "Label2"
-        Me.Label2.Size = New System.Drawing.Size(59, 20)
+        Me.Label2.Size = New System.Drawing.Size(42, 16)
         Me.Label2.TabIndex = 0
         Me.Label2.Text = "Name :"
         '
         'Label1
         '
         Me.Label1.AutoSize = True
-        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label1.Location = New System.Drawing.Point(288, 21)
+        Me.Label1.Font = New System.Drawing.Font("Bebas Neue", 48.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.ForeColor = System.Drawing.Color.FromArgb(CType(CType(192, Byte), Integer), CType(CType(0, Byte), Integer), CType(CType(0, Byte), Integer))
+        Me.Label1.Location = New System.Drawing.Point(212, 9)
         Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(127, 25)
+        Me.Label1.Size = New System.Drawing.Size(303, 77)
         Me.Label1.TabIndex = 4
         Me.Label1.Text = "Registration"
         '
         'btnReturn
         '
-        Me.btnReturn.Location = New System.Drawing.Point(153, 372)
+        Me.btnReturn.BackColor = System.Drawing.Color.FromArgb(CType(CType(192, Byte), Integer), CType(CType(0, Byte), Integer), CType(CType(0, Byte), Integer))
+        Me.btnReturn.FlatStyle = System.Windows.Forms.FlatStyle.Popup
+        Me.btnReturn.Font = New System.Drawing.Font("Arial Black", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnReturn.Location = New System.Drawing.Point(440, 378)
         Me.btnReturn.Name = "btnReturn"
         Me.btnReturn.Size = New System.Drawing.Size(126, 31)
         Me.btnReturn.TabIndex = 5
         Me.btnReturn.Text = "Return"
-        Me.btnReturn.UseVisualStyleBackColor = True
+        Me.btnReturn.UseVisualStyleBackColor = False
         '
         'btnRegister
         '
-        Me.btnRegister.Location = New System.Drawing.Point(435, 372)
+        Me.btnRegister.BackColor = System.Drawing.Color.FromArgb(CType(CType(192, Byte), Integer), CType(CType(0, Byte), Integer), CType(CType(0, Byte), Integer))
+        Me.btnRegister.FlatStyle = System.Windows.Forms.FlatStyle.Popup
+        Me.btnRegister.Font = New System.Drawing.Font("Arial Black", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnRegister.Location = New System.Drawing.Point(172, 378)
         Me.btnRegister.Name = "btnRegister"
         Me.btnRegister.Size = New System.Drawing.Size(126, 31)
         Me.btnRegister.TabIndex = 6
         Me.btnRegister.Text = "Register"
-        Me.btnRegister.UseVisualStyleBackColor = True
-        '
-        'txtName
-        '
-        Me.txtName.Location = New System.Drawing.Point(135, 31)
-        Me.txtName.Name = "txtName"
-        Me.txtName.Size = New System.Drawing.Size(305, 22)
-        Me.txtName.TabIndex = 5
-        '
-        'txtStaffID
-        '
-        Me.txtStaffID.Location = New System.Drawing.Point(135, 71)
-        Me.txtStaffID.Name = "txtStaffID"
-        Me.txtStaffID.Size = New System.Drawing.Size(305, 22)
-        Me.txtStaffID.TabIndex = 6
-        '
-        'txtPhoneNumber
-        '
-        Me.txtPhoneNumber.Location = New System.Drawing.Point(135, 112)
-        Me.txtPhoneNumber.Name = "txtPhoneNumber"
-        Me.txtPhoneNumber.Size = New System.Drawing.Size(305, 22)
-        Me.txtPhoneNumber.TabIndex = 7
-        '
-        'txtUsername
-        '
-        Me.txtUsername.Location = New System.Drawing.Point(135, 153)
-        Me.txtUsername.Name = "txtUsername"
-        Me.txtUsername.Size = New System.Drawing.Size(305, 22)
-        Me.txtUsername.TabIndex = 8
-        '
-        'txtPassword
-        '
-        Me.txtPassword.Location = New System.Drawing.Point(135, 219)
-        Me.txtPassword.Name = "txtPassword"
-        Me.txtPassword.Size = New System.Drawing.Size(305, 22)
-        Me.txtPassword.TabIndex = 9
-        '
-        'Label7
-        '
-        Me.Label7.AutoSize = True
-        Me.Label7.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label7.Location = New System.Drawing.Point(132, 244)
-        Me.Label7.Name = "Label7"
-        Me.Label7.Size = New System.Drawing.Size(140, 60)
-        Me.Label7.TabIndex = 10
-        Me.Label7.Text = "Password must contain :" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "  > Letter" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "  > A number" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "  > Length 8 - 12 " & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10)
+        Me.btnRegister.UseVisualStyleBackColor = False
         '
         'Registration
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.Black
         Me.ClientSize = New System.Drawing.Size(720, 421)
         Me.Controls.Add(Me.btnRegister)
         Me.Controls.Add(Me.btnReturn)
@@ -212,7 +209,6 @@ Partial Class Registration
     Friend WithEvents Label3 As Label
     Friend WithEvents btnReturn As Button
     Friend WithEvents btnRegister As Button
-    Friend WithEvents Label7 As Label
     Friend WithEvents txtPassword As TextBox
     Friend WithEvents txtUsername As TextBox
     Friend WithEvents txtPhoneNumber As TextBox
