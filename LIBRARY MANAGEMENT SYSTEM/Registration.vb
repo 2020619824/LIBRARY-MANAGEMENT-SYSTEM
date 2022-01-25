ef60f8ab97e15cd14d0cc042749efa9452bfd9a2
NURUL SYAQIRA BINTI ZAMRIDIN NURUL SYAQIRA BINTI ZAMRIDIN <2020457902@isiswa.uitm.edu.my>
2022-01-25 12:40:51 +0800

    registration

--- LIBRARY MANAGEMENT SYSTEM/Registration.vb
+++ LIBRARY MANAGEMENT SYSTEM/Registration.vb
@@ -1,6 +1,7 @@
 ﻿Imports System.Data.SqlClient
 
 Public Class Registration
+
     Private Sub Button2_Click(sender As Object, e As EventArgs)
         StartUp.Show()
         Me.Close()
@@ -28,32 +29,129 @@ Public Class Registration
             cmd.CommandText = "select * from Users where StaffID = ' " & txtStaffID.Text & "'"
             dr = cmd.ExecuteReader
             If dr.HasRows Then
-                MsgBox("Staff ID has been registered", MsgBoxStyle.Critical)
+                MyMessageBox.ShowMessage("Staff ID has been registered")
                 con.Close()
             Else
                 con.Close()
                 con.Open()
-                cmd = New SqlCommand("Insert into Users values(' " & txtName.Text & "','" & txtStaffID.Text & "','" & txtPhoneNumber.Text & "','" & txtUsername.Text & "','" & txtPassword.Text & "')", con)
+                cmd = New SqlCommand("Insert into Users values(' " & txtStaffID.Text & "','" & txtName.Text & "','" & txtPhoneNumber.Text & "','" & txtUsername.Text & "','" & txtPassword.Text & "')", con)
                 If (txtName.Text = "" And txtStaffID.Text = "" And txtPhoneNumber.Text = "" And txtUsername.Text = "" And txtPassword.Text = "") Then
-                    MessageBox.Show("Please enter the details")
+                    MyMessageBox.ShowMessage("Please enter the details")
                 Else
                     cmd.ExecuteNonQuery()
-                    MsgBox("Sucessfully Registered", MsgBoxStyle.Information, "Success")
-                    Me.Hide()
-                    Login.Show()
+                    MyMessageBox.ShowMessage("Sucessfully Registered")
                     txtName.Clear()
                     txtStaffID.Clear()
                     txtPhoneNumber.Clear()
                     txtUsername.Clear()
                     txtPassword.Clear()
-
+                    Me.Hide()
+                    Login.ShowDialog()
                 End If
                 con.Close()
             End If
             con.Close()
         Catch ex As Exception
-            MsgBox("Please Enter Correct Details")
+            MyMessageBox.ShowMessage("Connection Error")
         End Try
+    End Sub
+    Public Sub validatePassword()
+        Dim strUserPassword As String
+        strUserPassword = txtPassword.Text
+
+        Dim blnPasswordNotBlank As Boolean = False
+        Dim blnPasswordLengthValid As Boolean = False
+        Dim blnLettersAndNumbersOnly As Boolean = False
+        Dim blnPasswordContainsLetters As Boolean = False
+        Dim blnPasswordContainsNumbers As Boolean = False
+        Dim blnPasswordValid As Boolean = False
+
+        blnPasswordValid = False
+
+        If strUserPassword = Nothing Then
+            blnPasswordNotBlank = False
+            MyMessageBox.ShowMessage("Password Missing")
+            txtPassword.Focus()
+        Else
+            blnPasswordNotBlank = True
+        End If
+
+        If strUserPassword.Length < 8 Then
+            blnPasswordLengthValid = False
+            MyMessageBox.ShowMessage("Password must be between 8 to 12 charachters")
+            txtPassword.Focus()
+        Else
+            blnPasswordLengthValid = True
+        End If
+
+        Dim intPW_InvalidCharsCount As Integer = 0
+        Dim intPW_LettersAndNumbersCount As Integer = 0
+
+        For i = 0 To strUserPassword.Length - 1
+            If (Asc(strUserPassword.Substring(i, 1)) >= 65 _
+                And Asc(strUserPassword.Substring(i, 1)) <= 90) _
+                Or (Asc(strUserPassword.Substring(i, 1)) >= 97 _
+                And Asc(strUserPassword.Substring(i, 1)) <= 122) _
+                Or (Asc(strUserPassword.Substring(i, 1)) >= 48 _
+                And Asc(strUserPassword.Substring(i, 1)) <= 57) Then
+
+                intPW_LettersAndNumbersCount = intPW_LettersAndNumbersCount + 1
+            Else
+                intPW_InvalidCharsCount = intPW_InvalidCharsCount + 1
+                txtPassword.Focus()
+            End If
+
+            If intPW_InvalidCharsCount > 0 Then
+                blnLettersAndNumbersOnly = False
+                MyMessageBox.ShowMessage("Password must contain only letters and numbers only")
+                txtPassword.Focus()
+            Else
+                blnLettersAndNumbersOnly = True
+            End If
+        Next
+
+        Dim intPWNumbersCount As Integer = 0
+
+        For i = 0 To strUserPassword.Length - 1
+            If Asc(strUserPassword.Substring(i, 1)) >= 48 _
+                And Asc(strUserPassword.Substring(i, 1)) <= 57 Then
+                intPWNumbersCount = intPWNumbersCount + 1
+            End If
+        Next
+
+        If intPW_LettersAndNumbersCount = 0 Then
+            blnPasswordContainsLetters = False
+            MyMessageBox.ShowMessage("Password must contain Leter")
+            txtPassword.Focus()
+        Else
+            blnPasswordContainsLetters = True
+        End If
+
+        For i = 0 To strUserPassword.Length - 1
+            If Asc(strUserPassword.Substring(i, 1)) >= 48 _
+                And Asc(strUserPassword.Substring(i, 1)) <= 57 Then
+                intPWNumbersCount = intPWNumbersCount + 1
+            End If
+        Next
+
+        If intPWNumbersCount = 0 Then
+            blnPasswordContainsNumbers = False
+            MyMessageBox.ShowMessage("Password must contain at least one number")
+            txtPassword.Focus()
+        Else
+            blnPasswordContainsNumbers = True
+        End If
+
+        If blnPasswordNotBlank = True _
+            And blnPasswordLengthValid = True _
+            And blnLettersAndNumbersOnly = True _
+            And blnPasswordContainsLetters = True _
+            And blnPasswordContainsNumbers = True Then
+
+            blnPasswordValid = True
+
+        End If
+
     End Sub
 
     Private Sub btnReturn_Click(sender As Object, e As EventArgs) Handles btnReturn.Click
