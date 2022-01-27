71a906d8abf97daab20cd6582eb2c7326f367554
NURUL SYAQIRA BINTI ZAMRIDIN NURUL SYAQIRA BINTI ZAMRIDIN <2020457902@isiswa.uitm.edu.my>
2022-01-27 15:55:47 +0800

    book

--- LIBRARY MANAGEMENT SYSTEM/Book Issue Information.vb
+++ LIBRARY MANAGEMENT SYSTEM/Book Issue Information.vb
@@ -1,17 +1,3 @@
 ﻿Public Class BookIssueInformation
-    Private Sub DateTimePicker1_ValueChanged(sender As Object, e As EventArgs) Handles DateTimePicker1.ValueChanged
 
-    End Sub
-
-    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles Label1.Click
-
-    End Sub
-
-    Private Sub txtBorrower_TextChanged(sender As Object, e As EventArgs) Handles txtBorrower.TextChanged
-
-    End Sub
-
-    Private Sub btnReturn_Click(sender As Object, e As EventArgs) Handles btnReturn.Click
-
-    End Sub
 End Class
