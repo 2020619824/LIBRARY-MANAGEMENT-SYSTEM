4f36d695a4ae2896e6916024ed9142fe49da1856
NURUL AFIQAH BINTI MD NOR AZMAN NURUL AFIQAH BINTI MD NOR AZMAN <2020478448@isiswa.uitm.edu.my>
2022-02-01 02:24:40 +0800

    edit icon

--- LIBRARY MANAGEMENT SYSTEM/Login.Designer.vb
+++ LIBRARY MANAGEMENT SYSTEM/Login.Designer.vb
@@ -22,6 +22,7 @@ Partial Class Login
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Login))
         Me.Label1 = New System.Windows.Forms.Label()
         Me.Label2 = New System.Windows.Forms.Label()
         Me.txtGetUserName = New System.Windows.Forms.TextBox()
@@ -136,6 +137,7 @@ Partial Class Login
         Me.Controls.Add(Me.Label2)
         Me.Controls.Add(Me.Label1)
         Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
         Me.Name = "Login"
         Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
         Me.Text = "Login"
